966e668d63cce2d324368dbd56831c19cb213837
“RunaLeagit config --[200~global user.email “[valid-email]”git config --global user.name “RunaLea <runalea@outlook.com>
2025-01-10 22:35:05 +0100

    When you press the enter button the button is pressed

--- WordGame.js
+++ WordGame.js
@@ -2,4 +2,10 @@ function enterWord(){
     // Shows the typed word from the textbox in the console.
     let wordInput = document.getElementById("Textbox").value;
     console.log(wordInput);
+};
+function enterCheck(event){
+    let key = event.key;
+    if(key == "Enter"){
+        enterWord();
+    }
 }
