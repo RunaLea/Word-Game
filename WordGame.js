0456b309569d8384280d37f7fe789448995cbf88
“RunaLeagit config --[200~global user.email “[valid-email]”git config --global user.name “RunaLea <runalea@outlook.com>
2025-01-13 15:13:22 +0100

    You can no longer add a word that is too short or long.

--- WordGame.js
+++ WordGame.js
@@ -5,9 +5,7 @@ function enterWord(){
     // Shows the typed word from the textbox in the console.
     wordInput = document.getElementById("Textbox").value;
     console.log("Chosen word: ",wordInput);
-    turn += 1;
-    console.log("Turn: ",turn);
-    displayRow();
+    wordLenCheck();
     return wordInput;
 };
 function enterCheck(event){
@@ -17,12 +15,26 @@ function enterCheck(event){
         enterWord();
     }
 };
+function wordLenCheck(){
+    document.getElementById("textMessage").innerHTML = "";
+    // Checks if the input is 5 characters.
+    if(wordInput.length==5){
+        turn += 1;
+        console.log("Turn: ",turn);
+        displayRow();
+    }
+    else if(wordInput.length!=5){
+        document.getElementById("textMessage").innerHTML = "The word should be 5 characters.";
+    }
+}
 function displayRow(){ 
+    // Creates an array for the letters
     wordChoiceList = [];
     for (let x=0; x<wordInput.length; x++){
         wordChoiceList.push(wordInput[x]);
     };
     console.log(wordChoiceList);
+    // Places all letters in their respective letterboxes
     document.getElementById("letterBox"+(letterBoxCount+1)).innerHTML = wordChoiceList[0];
     document.getElementById("letterBox"+(letterBoxCount+2)).innerHTML = wordChoiceList[1];
     document.getElementById("letterBox"+(letterBoxCount+3)).innerHTML = wordChoiceList[2];
