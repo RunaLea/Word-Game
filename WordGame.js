d2909f2dba0ea80f9dcf9092a43077b26f662493
“RunaLeagit config --[200~global user.email “[valid-email]”git config --global user.name “RunaLea <runalea@outlook.com>
2025-01-13 16:36:34 +0100

    When you have 0 attempts you lose the game

--- WordGame.js
+++ WordGame.js
@@ -1,11 +1,13 @@
 let turn = 0;
 let letterBoxCount = 0
 let wordInput = "";
+let correctWord = "_____";
 function enterWord(){
     // Shows the typed word from the textbox in the console.
-    wordInput = document.getElementById("Textbox").value;
+    wordInput = document.getElementById("textBox").value;
     console.log("Chosen word: ",wordInput);
     wordLenCheck();
+    document.getElementById("textBox").value = "";
     return wordInput;
 };
 function enterCheck(event){
@@ -41,5 +43,27 @@ function displayRow(){
     document.getElementById("letterBox"+(letterBoxCount+4)).innerHTML = wordChoiceList[3];
     document.getElementById("letterBox"+(letterBoxCount+5)).innerHTML = wordChoiceList[4];
     letterBoxCount = (letterBoxCount+5);
+    turnCheck();
     return letterBoxCount;
+}
+function turnCheck(){
+    if(turn==5){
+        document.getElementById("gameResult").innerHTML = "You lost";
+        document.getElementById("textMessage").innerHTML = `The word was: ${correctWord.toUpperCase()}.`
+        document.getElementById("restartButton").style.display = "block";
+        document.getElementById("textBox").style.display = "none";
+        document.getElementById("enterButton").style.display = "none";
+    }
+}
+function restartGame(){
+    turn = 0
+    letterBoxCount = 0
+    for (let x=0; x<25; x++){
+        document.getElementById("letterBox"+(x+1)).innerHTML = "";
+    }
+    document.getElementById("gameResult").innerHTML = "";
+    document.getElementById("textMessage").innerHTML = "Try to guess the word in 5 attempts.";
+    document.getElementById("restartButton").style.display = "none";
+    document.getElementById("textBox").style.display = "inline-block";
+    document.getElementById("enterButton").style.display = "inline-block";
 }
